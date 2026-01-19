7b9b4375cd054c76c2e4854d04d6f66be4a09e31
Yatitooo <carlosantipascu@gmail.com>
2026-01-19 13:09:37 -0600

    entrega 1 area volumen

--- src/main.c
+++ src/main.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
 
-int main(int argc, char *argv)
+int suma(int x, int y) 
 {
-    printf("Hello world!\n");
-    return 0;
+int sum = x + y;
+    return sum;
+
+    
 }
+
+int multiplicacion(int x, int y) 
+{
+    int mul = x * y;
+    return x * y;
+
+}
+
+int area (int base, int altura) 
+{
+    return (base * altura) / 2;
+
+}
+
+
+int main() 
+{
+    int x = 0;
+    int y = 0;
+
+    printf( "enter the side length: %i\n", &x );
+    scanf( "%i", &x );
+
+    int a = area(x);
+
+   printf("area: %i\n", a);
+    return 0;
+
+} 
