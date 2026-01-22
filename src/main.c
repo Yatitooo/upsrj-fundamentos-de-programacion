e8a1c0290e5667c7eac66b40bb10fd8717ca3406
Yatitooo <carlosantipascu@gmail.com>
2026-01-22 11:03:24 -0600

    Entrega 2 par e impar

--- src/main.c
+++ src/main.c
@@ -1,18 +1,47 @@
 #include <stdio.h>
 
+//Funcion regresa 1 si el numero es par, 0 si es impar y 3 si es cero
+int is_even(int num) 
+{
+   if (num == 0)
+   {
+      return 3; //cero
+   }
+   else 
+   {
+    if (num % 2 == 0) 
+    {
+        return 1; //es par
+    } 
+    else if (num % 2 != 0)
+    {
+        return 0; //es impar
+    }
+   }
+    
+}
+
 int main()
 {
-   float side;
-   
+   int num;
 
-    printf( "Enter the side length:");
-    scanf( "%f", &side);
+    //para solicitar un numero
+    printf( "Ingresa un Numero:");
+    scanf( "%d" , &num);
     
-    float area = 6 * side * side;
-    float volume = side * side * side;
+        if (is_even(num) == 1) //Verifica si el numero es par
+        {
+        printf("El numero es par\n");
+        } 
+        else if (is_even(num) == 0) //Verifica si el numero es impar
+        {
+        printf("El numero es impar\n");
+        }
+        else if (is_even(num) == 3) //Verifica si el numero es cero
+        {
+        printf("El numero es cero\n");
+        }
 
-   printf("Square area: %f\n", area);
-    printf("Cube volume: %f\n", volume);
     return 0;
+}
 
-} 
